7f5f548fb840238b5e7a8d602cb9c752ba239ffe
unknown <mahmoud.h.alshukri@gmail.com>
2023-01-29 11:36:20 +0200

    add _redirects

--- src/state/api.js
+++ src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://mernstack-dashboard-backend.onrender.com'}),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL}),
     reducerPath: 'adminApi',
     tagTypes: ['User', 'Products', 'Customers', 'Transactions', 'Geography', 'Sales', 'Admins', 'Performance', 'Dashboard'],
     endpoints: (build) => ({
